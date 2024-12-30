797ec5f64037a42d087f6a40a80a1ee4eaa9d58c
Niharika Upadhyayula <niharika.u03@gmail.com>
2024-12-30 16:44:56 -0600

    landing-page

---
+++ src/pages/home/home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Home.css';
+
+export default function Home() {
+  return (
+    <div className="container">
+      <div className="leftSection">
+        <h1 className="heading">
+          Hi! This is Niharika, a website designer and developer
+        </h1>
+        <p className="description">
+        I love creating websites that blend seamless functionality with striking visuals, 
+        making them a joy to explore. 
+        My passion lies in crafting user-friendly experiences and diving into the world of digital marketing. 
+        I’m constantly inspired by unique marketing ideas in everyday life, 
+        believing in their power to elevate brands and small projects to extraordinary heights.
+        </p>
+        <button className="button-primary">Get in Touch →</button>
+      </div>
+      <div className="rightSection">
+        <img
+          src={`${process.env.PUBLIC_URL}/computer-work.gif`}
+          alt="Computer Work"
+          className="image"
+        />
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/projects/project-1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Project1() {
+  return (
+    <div>
+      <h1>Projects Page</h1>
+      <p>This is the projects page.</p>
+    </div>
+  );
+}
+
+export default Project1;
