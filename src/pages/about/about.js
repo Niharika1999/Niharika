a4d5b64354c7ba1baaf751b32323c189aabdae94
Niharika Upadhyayula <niharika.u03@gmail.com>
2024-12-30 19:50:53 -0600

    about-intial
    fill up the empty space

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
+import './about.css';
+
+const Experience = lazy(() => import('./experience'));
+const Skills = lazy(() => import('./skills/skills'));
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is the about page.</p>
+    <div className="about-container">
+    <div className="container">
+      <div className="leftSection">
+        <h1 className="heading">About Me</h1>
+        <p className="description">
+          I love creating websites that blend seamless functionality with striking visuals, 
+          making them a joy to explore. 
+          My passion lies in crafting user-friendly experiences and diving into the world of digital marketing. 
+          I’m constantly inspired by unique marketing ideas in everyday life, 
+          believing in their power to elevate brands and small projects to extraordinary heights.
+        </p>
+        <p>I am dedicated to expanding my knowledge and expertise in my field. Throughout my journey, I've acquired various skills, which I continue to perfect.</p>
+        <p>I dream of contributing to projects that not only look good but also do good — for people who engage with the applications. I'm also keen on exploring how AI can further personalize user experiences without losing the human touch.</p>
+        <hr></hr>
+        <h2>My Tool Kit</h2>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Skills />
+        </Suspense>
+        <hr></hr>
+        <h2>My Experiences</h2>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Experience />
+        </Suspense>
+      </div>
+      <div className="rightSection">
+        <img
+          src={`${process.env.PUBLIC_URL}/about.png`}
+          alt="About"
+          className="image"
+          loading="lazy"
+        />
+      </div>
+    </div>
+    
     </div>
   );
 }
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -24,6 +24,7 @@ export default function Home() {
           className="image"
         />
       </div>
+     
     </div>
   );
 }
